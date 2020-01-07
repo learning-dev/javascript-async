623ab47a976f82f0c93feced5560c4c4fbcd28f7
learning-dev <ayazm.140895@gmail.com>
2020-01-07 18:34:57 +0530

    task1 and task2 for trello-promises

--- trello-promises.js
+++ trello-promises.js
@@ -1,10 +1,10 @@
 function getBoard() {
   return new Promise((resolve, reject) => {
     console.log('Fetching board...');
-    setTimeout(function() {
-      let board = {
-        id: "def453ed",
-        name: "Thanos"
+    setTimeout(() => {
+      const board = {
+        id: 'def453ed',
+        name: 'Thanos',
       };
       console.log('Received board');
       resolve(board);
@@ -15,34 +15,34 @@ function getBoard() {
 function getLists(boardId) {
   return new Promise((resolve, reject) => {
     console.log(`Fetching lists for board id ${boardId}...`);
-    setTimeout(function() {
-      let lists = {
+    setTimeout(() => {
+      const lists = {
         def453ed: [
           {
-            id: "qwsa221",
-            name: "Mind"
+            id: 'qwsa221',
+            name: 'Mind',
           },
           {
-            id: "jwkh245",
-            name: "Space"
+            id: 'jwkh245',
+            name: 'Space',
           },
           {
-            id: "azxs123",
-            name: "Soul"
+            id: 'azxs123',
+            name: 'Soul',
           },
           {
-            id: "cffv432",
-            name: "Time"
+            id: 'cffv432',
+            name: 'Time',
           },
           {
-            id: "ghnb768",
-            name: "Power"
+            id: 'ghnb768',
+            name: 'Power',
           },
           {
-            id: "isks839",
-            name: "Reality"
-          }
-        ]
+            id: 'isks839',
+            name: 'Reality',
+          },
+        ],
       };
       console.log(`Received lists for board id ${boardId}`);
       resolve(lists[boardId]);
@@ -53,70 +53,70 @@ function getLists(boardId) {
 function getCards(listId) {
   return new Promise((resolve, reject) => {
     console.log(`Fetching cards for list id ${listId}...`);
-    setTimeout(function() {
-      let cards = {
+    setTimeout(() => {
+      const cards = {
         qwsa221: [
           {
-            id: "1",
-            description: `Having acquired the Power Stone, one of the six Infinity Stones,from the planet Xandar`
+            id: '1',
+            description: 'Having acquired the Power Stone, one of the six Infinity Stones,from the planet Xandar',
           },
           {
-            id: "2",
-            description: `Having acquired the Power Stone, one of the six Infinity Stones,from the planet Xandar`
+            id: '2',
+            description: 'Having acquired the Power Stone, one of the six Infinity Stones,from the planet Xandar',
           },
           {
-            id: "3",
-            description: `Having acquired the Power Stone, one of the six Infinity Stones,from the planet Xandar`
-          }
+            id: '3',
+            description: 'Having acquired the Power Stone, one of the six Infinity Stones,from the planet Xandar',
+          },
         ],
         jwkh245: [
           {
-            id: "1",
-            description: `intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.`
+            id: '1',
+            description: 'intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.',
           },
           {
-            id: "2",
-            description: `intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.`
+            id: '2',
+            description: 'intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.',
           },
           {
-            id: "3",
-            description: `intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.`
+            id: '3',
+            description: 'intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.',
           },
           {
-            id: "4",
-            description: `intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.`
-          }
+            id: '4',
+            description: 'intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.',
+          },
         ],
         azxs123: [
           {
-            id: "1",
-            description: `intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.`
+            id: '1',
+            description: 'intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.',
           },
           {
-            id: "2",
-            description: `intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.`
-          }
+            id: '2',
+            description: 'intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.',
+          },
         ],
         cffv432: [
           {
-            id: "1",
-            description: `intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.`
+            id: '1',
+            description: 'intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.',
           },
           {
-            id: "2",
-            description: `intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.`
-          }
+            id: '2',
+            description: 'intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.',
+          },
         ],
         ghnb768: [
           {
-            id: "1",
-            description: `intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.`
+            id: '1',
+            description: 'intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.',
           },
           {
-            id: "2",
-            description: `intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.`
-          }
-        ]
+            id: '2',
+            description: 'intercept a spaceship carrying the surviving Asgardians. As they extract the Space Stone from the Tesseract, Thanos subdues Thor, overpowers Hulk, and kills Heimdall and Loki.',
+          },
+        ],
       };
       console.log(`Received cards for list id ${listId}`);
       resolve(cards[listId] || []);
@@ -127,3 +127,40 @@ function getCards(listId) {
 // Task 1 board -> lists -> cards for list qwsa221
 // Task 2 board -> lists -> cards for list qwsa221 and cards for list jwkh245 simultaneously
 // Task 3 board -> lists -> cards for all lists simultaneously
+
+// const task1 = getBoard()
+//   .then((board) => {
+//     getLists(board.id)
+//       .then((list) => {
+//       // console.log('list', list);
+//         getCards(list[0].id)
+//           .then((cards) => {
+//             console.log('\n Task 1');
+//             console.log('list', list[0].id);
+//             console.log(cards);
+//           });
+//       });
+//   });
+
+
+const task2 = getBoard()
+  .then((board) => {
+    getLists(board.id)
+      .then((list) => {
+      // console.log('list', list);
+      console.log('\n Task 2');
+        getCards(list[0].id)
+          .then((cards) => {
+            console.log('list', list[0]);
+            console.log(cards);
+          });
+        getCards(list[1].id)
+          .then((cards) => {
+            console.log('list', list[1]);
+            console.log(cards);
+          });
+      });
+  });
+
+
+
