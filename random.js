4802b1ec6058fd358c61d0cd1908f62d1b474751
learning-dev <ayazm.140895@gmail.com>
2020-01-07 20:00:58 +0530

    add task 1 for random.js

--- random.js
+++ random.js
@@ -1,27 +1,31 @@
-function fetchRandomNumbers(callback){
-    console.log('Fetching number...');
+function fetchRandomNumbers() {
+  console.log('Fetching number...');
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-        let randomNum = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
-        console.log('Received random number:', randomNum);
-        callback(randomNum);
+      const randomNum = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
+      console.log('Received random number:', randomNum);
+      resolve(randomNum);
     }, (Math.floor(Math.random() * (5)) + 1) * 1000);
-}
-
-function fetchRandomString(callback){
-    console.log('Fetching string...');
-    setTimeout(() => {
-        let result           = '';
-        let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        let charactersLength = characters.length;
-        for ( let i = 0; i < 5; i++ ) {
-           result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        console.log('Received random string:', result);
-        callback(result);
-    }, (Math.floor(Math.random() * (5)) + 1) * 1000);
+  });
 }
 
 
+function fetchRandomString(callback) {
+  console.log('Fetching string...');
+  setTimeout(() => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < 5; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    console.log('Received random string:', result);
+    callback(result);
+  }, (Math.floor(Math.random() * (5)) + 1) * 1000);
+}
 
-fetchRandomNumbers((randomNum) => console.log(randomNum))
-fetchRandomString((randomStr) => console.log(randomStr))
+
+fetchRandomNumbers().then((randomNum) => {
+  console.log(randomNum);
+});
+fetchRandomString((randomStr) => console.log(randomStr));
